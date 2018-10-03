c59d5a972ed08c580b8cd3e7e2f9a468d6aab649
Nadim Mahmud <37373290+Nadim-Mahmud@users.noreply.github.com>
2018-10-03 13:20:59 +0600

    Update Main.java
    iteration mathod added

--- RootFindingMathod/src/Main.java
+++ RootFindingMathod/src/Main.java
@@ -5,10 +5,13 @@ public class Main {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         Bisection BS = new Bisection();
-
+        IterationMathod it = new IterationMathod();
        // double ans = ;
         
-        System.out.println("\n\nRoot : " + BS.bisection());
-        System.out.println("\nNumber of iteration : " + BS.getIteration());
+        System.out.println("Root : " + BS.bisection());
+        System.out.println("Number of iteration : " + BS.getIteration());
+        
+        System.out.println("\n\nIteration Mathod-->");
+        System.out.println("Root : " + it.iteration());
     }
 }
