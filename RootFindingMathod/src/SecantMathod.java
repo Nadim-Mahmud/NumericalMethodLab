59d9a632f6ddcde384433bc50bec54c91aceeeaf
Nadim Mahmud <37373290+Nadim-Mahmud@users.noreply.github.com>
2018-10-10 13:16:43 +0600

    Add files via upload
    Secant mathod class added

---
+++ RootFindingMathod/src/SecantMathod.java
@@ -0,0 +1,35 @@
+
+public class SecantMathod {
+   
+	public double tolarance = 0.00001;
+	
+	Equation eq = new Equation();
+	RandomNumber rn = new  RandomNumber(); 
+	
+	
+	public double secant() {
+		
+		double x1 = 2;//rn.positivRnd();
+		double x2 = 3;//rn.negetiveRnd();
+		double x3,x4,x0,dv;
+		int cn = 0;
+		
+		//System.out.println(x1 + " " + x2);
+		
+		while(Math.abs(x2-x1)>tolarance) {
+			
+			dv = (eq.equation(x2)-eq.equation(x1))/(x2-x1);
+	
+			x0 = x2 - eq.equation(x2)/Math.abs(dv);
+			
+			x1 = x2;
+			x2 = x0;
+			cn++;
+			///System.out.println(x1 + " " + x2);
+		}
+		System.out.println(cn);
+		
+		return x2;
+	}
+   
+} 
