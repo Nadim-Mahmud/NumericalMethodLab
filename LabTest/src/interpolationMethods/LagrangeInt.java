bd12ff198396d841db232802b47db72e0acdd03d
Nadim-Mahmud <mahmudnadim16@gmail.com>
2019-02-12 23:46:37 +0600

    Lagrange interpolation

--- LabTest/src/interpolationMethods/LagrangeInt.java
+++ LabTest/src/interpolationMethods/LagrangeInt.java
@@ -1,5 +1,49 @@
 package interpolationMethods;
 
-public class LagrangeInt {
+import java.util.Scanner;
 
+public class LagrangeInt {
+	
+	public double [] xv = new double [1000];
+	public double [] yv = new double [1000];
+	public int points;
+	public double targetx;
+	public String str;
+	
+	public LagrangeInt() {
+		
+		Scanner sc = new Scanner(System.in);
+		points = sc.nextInt();
+		
+		str = sc.next();
+		targetx = Double.parseDouble(str);
+		
+		for(int i=0; i<points; i++) {
+			str = sc.next();
+			xv[i] = Double.parseDouble(str);
+			str = sc.next();
+			yv[i] = Double.parseDouble(str);
+		}
+		
+		sc.close();
+	}
+	
+	public double calculate() {
+		
+		double tmp,sum=0;
+		
+		for(int i=0; i<points ; i++) {
+			
+			tmp = yv[i];
+			for(int j=0; j<points; j++) {
+				
+				if(i!=j) {
+					tmp = tmp*(targetx - xv[j])/(xv[i] - xv[j]);
+				}
+			}
+			sum += tmp;
+		}
+		return sum;
+	}
 }
+
--- LabTest/src/interpolationMethods/Main.java
+++ LabTest/src/interpolationMethods/Main.java
@@ -3,10 +3,12 @@ package interpolationMethods;
 public class Main {
 
 	public static void main(String[] args) {
+		//To get ans make comment any one of them here i am using lagrange
 		
-		NewtonsForwordInterpolation newton = new NewtonsForwordInterpolation();
+		//NewtonsForwordInterpolation newton = new NewtonsForwordInterpolation();
+		//System.out.println("Newtons forword difference interpolation : " + newton.calculate());
 		
-		System.out.println("Newtons forword difference interpolation : " + newton.calculate());
+		System.out.println("Lagrange Interpoaltion : "+ new LagrangeInt().calculate());
 	}
 
 }
